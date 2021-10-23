f043d77e4c4abf1a95d085cd7cf4fad7c5173f55
Kevin Collazos <collazo-s@hotmail.com>
2021-10-22 22:05:38 -0400

    map added

---
+++ config/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDSJrCRxR4EBftxMiVWHKiaccr0D6RHB00",
+  authDomain: "interactive-map-a0add.firebaseapp.com",
+  projectId: "interactive-map-a0add",
+  storageBucket: "interactive-map-a0add.appspot.com",
+  messagingSenderId: "785993436560",
+  appId: "1:785993436560:web:5ddea3d62c158372f1b715",
+  measurementId: "G-PG2TDDB6VT",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
